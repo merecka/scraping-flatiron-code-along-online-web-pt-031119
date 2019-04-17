6bd775e6068aee2c38c00812d6c2d353477d5377
Alex Merecka <merecka@gmail.com>
2019-04-17 16:11:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,8 +16,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
